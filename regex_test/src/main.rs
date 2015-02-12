ac7f7a84baf3ddd4000165dbd435f4700a7065b6
Geoffry Song <goffrie@gmail.com>
2015-02-11 21:31:05 -0500

    Fix regex_test.

--- regex_test/src/main.rs
+++ regex_test/src/main.rs
@@ -6,6 +6,8 @@ extern crate dfagen;
 extern crate syntax;
 
 use regex_dfa::{Regex, Dfa};
+use regex_dfa::dfa::Normalize;
+use regex_dfa::derivatives::Differentiable;
 
 fn main() {
     while let Ok(line) = std::io::stdio::stdin().read_line() {
@@ -13,13 +15,13 @@ fn main() {
             Err(e) => println!("error: {:?}", e),
             Ok(x) => {
                 println!("ok: {:?}", x);
-                let x = x.simplify();
+                let x = x.normalize();
                 println!("{:?}", x);
-                println!("{:?}", x.derivative().map(regex_dfa::regex::Regex::simplify));
-                let dfa = Dfa::from_regex(x);
+                println!("{:?}", x.derivative().map(Normalize::normalize));
+                let dfa = Dfa::from_derivatives(vec![x, Regex::Null]);
                 println!("{:?}", dfa);
                 println!("{}", syntax::print::pprust::item_to_string(
-                        &dfagen::dfagen(&dfa, syntax::parse::token::str_to_ident("dfa"), syntax::ast::Visibility::Public, syntax::codemap::DUMMY_SP))
+                        &*dfagen::dfagen(&dfa.0, syntax::parse::token::str_to_ident("dfa"), syntax::ast::Visibility::Public, syntax::codemap::DUMMY_SP))
                     );
             }
         }
