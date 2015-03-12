87a303059e12d740ed63cda50a884c0ad048c404
Geoffry Song <goffrie@gmail.com>
2015-03-11 23:36:19 -0400

    Add some more testcases.

---
+++ custom_testcases/a3/J1_Char_Index.java
@@ -0,0 +1,8 @@
+public class J1_Char_Index {
+    public static int test() {
+        char x = '\123';
+        int[] array = new int[x];
+        array[x] = 123;
+        return 0;
+    }
+}
---
+++ custom_testcases/a3/Je_Widening.java
@@ -0,0 +1,7 @@
+public class Je_Widening {
+    public static int test() {
+        byte a = (byte) 2;
+        byte b = (byte) 3;
+        byte x = a * b; //~ error: cast required for narrowing conversion from `int` to `byte`
+    }
+}
