e51582acabaa2f0bc2b79f935ebb84f396ffaf94
Geoffry Song <goffrie@gmail.com>
2015-03-17 16:57:35 -0400

    Properly evaluate constant expressions.
    This passes all A4 test cases.

--- ast/src/eval.rs
+++ ast/src/eval.rs
@@ -1,41 +1,162 @@
 use middle::*;
 use ast;
 
-pub fn eval_const_bool<'a, 'ast>(expr: &TypedExpression<'a, 'ast>) -> Option<bool> {
+#[derive(Show, Clone)]
+pub enum Value {
+    Int(i32),
+    Short(i16),
+    Char(i16), // a UTF-16 code unit
+    Byte(i8),
+    Bool(bool),
+    String(String),
+}
+
+macro_rules! try_opt {
+    ($e: expr) => (match $e { Some(v) => v, None => return None });
+}
+
+pub fn eval_const<'a, 'ast>(expr: &TypedExpression<'a, 'ast>) -> Option<Value> {
     use middle::TypedExpression_::*;
-    if !matches!(Type::SimpleType(SimpleType::Boolean), *expr.ty()) {
-        return None;
-    }
-    match expr.0 {
+    use self::Value::*;
+    let r = match expr.0 {
         Literal(lit) => {
             match *lit {
-                ast::Literal::Boolean(v) => Some(v),
-                _ => panic!("non-boolean literal given a boolean type"),
+                ast::Literal::Integer(v) => Int(v as i32),
+                ast::Literal::Boolean(v) => Bool(v),
+                ast::Literal::Character(v) => Char(v as u32 as i16),
+                ast::Literal::String(ref v) => String(v.clone()),
+                _ => return None,
             }
         }
         Prefix(op, box ref inner) => {
             use ast::PrefixOperator::*;
-            eval_const_bool(inner).map(|v| {
-                match op {
-                    Not => !v,
-                    _ => panic!("invalid boolean operator"),
-                }
-            })
+            match (op, try_opt!(eval_const(inner))) {
+                (Not, Bool(v)) => Bool(!v),
+                (Not, _) => panic!("bad Not"),
+                (Minus, Int(v)) => Int(-v),
+                (Minus, Short(_)) |
+                (Minus, Byte(_)) => panic!("unpromoted Minus operand"),
+                (Minus, _) => panic!("bad Minus"),
+            }
         }
         Infix(op, box ref left, box ref right) => {
             use ast::InfixOperator::*;
-            eval_const_bool(left).and_then(|l| {
-                eval_const_bool(right).and_then(|r| {
-                    match op {
-                        Xor => Some(l ^ r),
-                        EagerOr | LazyOr => Some(l || r),
-                        EagerAnd | LazyAnd => Some(l && r),
-                        // FIXME: Handle more constant expressions
-                        _ => None,
-                    }
-                })
+            match (op, try_opt!(eval_const(left)), try_opt!(eval_const(right))) {
+                (Xor, Bool(l), Bool(r)) => Bool(l ^ r),
+                (Xor, _, _) => panic!("bad Xor"),
+                (EagerOr, Bool(l), Bool(r)) |
+                (LazyOr, Bool(l), Bool(r)) => Bool(l || r),
+                (EagerOr, _, _) |
+                (LazyOr, _, _) => panic!("bad Or"),
+                (EagerAnd, Bool(l), Bool(r)) |
+                (LazyAnd, Bool(l), Bool(r)) => Bool(l && r),
+                (EagerAnd, _, _) |
+                (LazyAnd, _, _) => panic!("bad And"),
+
+                (Equals, Bool(l), Bool(r)) => Bool(l == r),
+                (Equals, Int(l), Int(r)) => Bool(l == r),
+                (Equals, String(l), String(r)) => Bool(l == r),
+                (Equals, _, _) => panic!("bad Equals"),
+                (NotEquals, Bool(l), Bool(r)) => Bool(l != r),
+                (NotEquals, Int(l), Int(r)) => Bool(l != r),
+                (NotEquals, String(l), String(r)) => Bool(l != r),
+                (NotEquals, _, _) => panic!("bad NotEquals"),
+
+                (LessThan, Int(l), Int(r)) => Bool(l < r),
+                (LessThan, _, _) => panic!("bad LessThan"),
+                (GreaterThan, Int(l), Int(r)) => Bool(l > r),
+                (GreaterThan, _, _) => panic!("bad GreaterThan"),
+                (LessEqual, Int(l), Int(r)) => Bool(l <= r),
+                (LessEqual, _, _) => panic!("bad LessEqual"),
+                (GreaterEqual, Int(l), Int(r)) => Bool(l >= r),
+                (GreaterEqual, _, _) => panic!("bad GreaterEqual"),
+
+                (Plus, Int(l), Int(r)) => Int(l + r),
+                (Plus, String(l), String(r)) => String(l + &*r),
+                (Plus, _, _) => panic!("bad Plus"),
+                (Minus, Int(l), Int(r)) => Int(l - r),
+                (Minus, _, _) => panic!("bad Minus"),
+                (Mult, Int(l), Int(r)) => Int(l * r),
+                (Mult, _, _) => panic!("bad Mult"),
+
+                (Div, Int(_), Int(0)) |
+                (Modulo, Int(_), Int(0)) => {
+                    span_warning!(right.span, "division by zero");
+                    return None
+                }
+                (Div, Int(l), Int(r)) => {
+                    // Explicitly use 2's-complement overflow.
+                    let v = l as i64 / r as i64;
+                    Int(v as i32)
+                }
+                (Div, _, _) => panic!("bad Div"),
+                (Modulo, Int(l), Int(r)) => {
+                    let v = l as i64 % r as i64;
+                    Int(v as i32)
+                }
+                (Modulo, _, _) => panic!("bad Modulo"),
+            }
+        }
+        Cast(_, box ref inner) |
+        Widen(box ref inner) => {
+            let val = try_opt!(eval_const(inner));
+            // To avoid combinatorial blowup, always convert numberic types to Int
+            let val = match val {
+                Int(v) => Int(v),
+                Short(v) => Int(v as i32),
+                Char(v) => Int(v as i32),
+                Byte(v) => Int(v as i32),
+                v => v,
+            };
+            match (expr.ty(), val) {
+                (&Type::SimpleType(SimpleType::Int), Int(v)) => Int(v),
+                (&Type::SimpleType(SimpleType::Int), _) => panic!("bad cast to int"),
+                (&Type::SimpleType(SimpleType::Short), Int(v)) => Short(v as i16),
+                (&Type::SimpleType(SimpleType::Short), _) => panic!("bad cast to short"),
+                (&Type::SimpleType(SimpleType::Char), Int(v)) => Char(v as i16),
+                (&Type::SimpleType(SimpleType::Char), _) => panic!("bad cast to char"),
+                (&Type::SimpleType(SimpleType::Byte), Int(v)) => Byte(v as i8),
+                (&Type::SimpleType(SimpleType::Byte), _) => panic!("bad cast to byte"),
+                (&Type::SimpleType(SimpleType::Boolean), Bool(v)) => Bool(v),
+                (&Type::SimpleType(SimpleType::Boolean), _) => panic!("bad cast to bool"),
+                (_, _) => return None
+            }
+        }
+        ToString(box ref inner) => {
+            String(match try_opt!(eval_const(inner)) {
+                Int(v) => format!("{}", v),
+                Short(v) => format!("{}", v),
+                Char(v) => format!("{}", try_opt!(::std::char::from_u32(v as u32))),
+                Byte(v) => format!("{}", v),
+                Bool(v) => format!("{}", v),
+                String(v) => v,
             })
         }
-        _ => None
+        _ => return None,
+    };
+    // sanity check
+    match (&r, expr.ty()) {
+        (&Int(_), &Type::SimpleType(SimpleType::Int)) |
+        (&Short(_), &Type::SimpleType(SimpleType::Short)) |
+        (&Char(_), &Type::SimpleType(SimpleType::Char)) |
+        (&Byte(_), &Type::SimpleType(SimpleType::Byte)) |
+        (&Bool(_), &Type::SimpleType(SimpleType::Boolean)) => (),
+        (&String(_), &Type::SimpleType(SimpleType::Other(tydef))) => {
+            // hack alert
+            assert_eq!(tydef.fq_name.to_string(), "java.lang.String")
+        }
+        (r, ty) => {
+            panic!("sanity check failed: constant expression evaluated to {:?}, but had type {}",
+                   r, ty)
+        }
+    }
+    Some(r)
+}
+
+pub fn eval_const_bool<'a, 'ast>(expr: &TypedExpression<'a, 'ast>) -> Option<bool> {
+    if let Some(Value::Bool(v)) = eval_const(expr) {
+        Some(v)
+    } else {
+        None
     }
 }
---
+++ custom_testcases/a4/J1_Constant_Eval.java
@@ -0,0 +1,31 @@
+public class J1_Constant_Eval {
+    public J1_Constant_Eval() {}
+    public static int test() {
+        if (true) while (1 == 1);
+        else if (true) while (1 != 2);
+        else if (true) while (1 < 2);
+        else if (true) while (2 <= 2);
+        else if (true) while (-123 >= -1234 && 3 > -0);
+        else if (true) while (-123 * -123 == 15129);
+        else if (true) while (33 / 7 == 4 && 33 / -7 == -4 && -33 / -7 == 4);
+        else if (true) while (false || "asdf" == "asdf");
+        else if (true) while ((short) 123 == 123 && (char) 123 == 123);
+        else if (true) while (65 == 'A');
+        else if (true) while ((byte) 123 == (short) 123);
+        else if (true) while ((byte) 256 == (char) 0);
+        else if (true) while ((short) 65536 == (byte) 512);
+        else if (true) while (65536 * 32768 == -2147483648);
+        else if (true) while (65536 * 65536 == 0);
+        else if (true) while ((-1) * -2147483648 == -2147483648 && -(-2147483648) == -2147483648 && (-2147483648) / -1 == -2147483648);
+        else if (true) while ((-123) % 12 == -3);
+        else if (true) while ((-2147483648) % -1 == 0);
+        else if (true) while ("AB" == "AB");
+        else if (true) while ("AB" == "A"+"B");
+        else if (true) while ("AB" == 'A'+"B");
+        else if (true) while ("AB" == "A"+'B');
+        else if (true) while ("AB123" == "A"+"B"+123 && "AB123" == 'A'+"B"+(short)123 && "AB" == (char)65+"B");
+        else if (true) while ("AB67" == "AB"+('A'+2) && "ABA2" == "AB"+'A'+2);
+        else if (true) while ("-1" == ""+(byte)255);
+        else while (true);
+    }
+}
